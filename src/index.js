ab6cd4b120bbdb36bae64430fc92897e73512c7d
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2023-03-09 09:32:41 -0300

    chore: add bodyParser

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ require('dotenv').config()
 
 const express = require('express')
 const loginRoutes = require('./routes/login')
+const bodyParser = require('body-parser')
 
 const app = express()
-app.use(express.json())
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.use('/login-test', loginRoutes)
 // app.use('/test', testRoutes)
 
