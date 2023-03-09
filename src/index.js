ec77775d340a4eb3f93c7df9137e2c084a1eca83
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2023-03-09 09:20:55 -0300

    chore: some changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const loginRoutes = require('./routes/login')
 
 const app = express()
 app.use(express.json())
-app.use('/test', loginRoutes)
+app.use('/login-test', loginRoutes)
 // app.use('/test', testRoutes)
 
 const PORT = process.env.PORT || 3000
@@ -13,6 +13,29 @@ const PORT = process.env.PORT || 3000
 app.get('/ping', (req, res) => {
   console.log('someone pinged here!!')
   res.send('pong')
+})
+
+app.post('/login', (req, res) => {
+  // console.log({ env: process.env })
+  // const { email, password } = req.body
+
+  // TODO: create method to validate credentials
+  // const token = jwt.sign(
+  //   { email, password },
+  //   process.env.SECRET,
+  //   {
+  //     expiresIn: '1m'
+  //   }
+  // )
+
+  // res.json({ email, token })
+  console.log('login post')
+  res.json({ message: 'login post' })
+})
+
+app.get('/login', (req, res) => {
+  console.log('login get')
+  res.json({ message: 'login get' })
 })
 
 app.listen(PORT, () => {
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,22 +1,29 @@
 const express = require('express')
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
 const router = express.Router()
 
 router.post('/', (req, res) => {
-  console.log({ env: process.env })
-  const { email, password } = req.body
+  // console.log({ env: process.env })
+  // const { email, password } = req.body
 
   // TODO: create method to validate credentials
-  const token = jwt.sign(
-    { email, password },
-    process.env.SECRET,
-    {
-      expiresIn: '1m'
-    }
-  )
+  // const token = jwt.sign(
+  //   { email, password },
+  //   process.env.SECRET,
+  //   {
+  //     expiresIn: '1m'
+  //   }
+  // )
 
-  res.json({ email, token })
+  // res.json({ email, token })
+  console.log('login post')
+  res.json({ message: 'login post' })
+})
+
+router.get('/', (req, res) => {
+  console.log('login get')
+  res.json({ message: 'login get' })
 })
 
 module.exports = router
