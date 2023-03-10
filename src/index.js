7642df541acfde98bb3364126ff30271f04ba61d
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2023-03-09 21:11:47 -0300

    chore: integrate all in index.js

--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
 require('dotenv').config()
 
 const express = require('express')
-const loginRoutes = require('./routes/login')
 const bodyParser = require('body-parser')
+const { validateToken } = require('./routes/middlewares/jwt')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use('/login-test', loginRoutes)
-// app.use('/test', testRoutes)
+// Routes
 
-const PORT = process.env.PORT || 3000
+// Login
+app.post('/login', (req, res) => {
+  console.log({ env: process.env })
+  const { email, password } = req.body
 
+  // TODO: create method to validate credentials
+  const token = jwt.sign(
+    { email, password },
+    process.env.SECRET,
+    {
+      expiresIn: '1m'
+    }
+  )
+
+  res.json({ email, token })
+})
+
+// Test
+app.get('/test', validateToken, (_req, res) => {
+  res.json({ test: 'test' })
+})
+
+// Ping
 app.get('/ping', (req, res) => {
   console.log('someone pinged here!!')
   res.send('pong')
 })
 
-app.post('/login', (req, res) => {
-  // console.log({ env: process.env })
-  // const { email, password } = req.body
-
-  // TODO: create method to validate credentials
-  // const token = jwt.sign(
-  //   { email, password },
-  //   process.env.SECRET,
-  //   {
-  //     expiresIn: '1m'
-  //   }
-  // )
-
-  // res.json({ email, token })
-  console.log('login post')
-  res.json({ message: 'login post' })
-})
-
-app.get('/login', (req, res) => {
-  console.log('login get')
-  res.json({ message: 'login get' })
-})
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
